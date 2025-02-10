32d6081c2c1f2ae2eadb0a721e85204aa02cd460
Daniel Nold <75209804+Dnold@users.noreply.github.com>
2025-02-10 22:40:51 +0100

    Added Trader UI

--- Prototype/Assets/Scripts/Trader/Trader.cs
+++ Prototype/Assets/Scripts/Trader/Trader.cs
@@ -8,61 +8,30 @@ public class Trader : MonoBehaviour
 {
     public List<UpgradeBase> availableUpgrades = new List<UpgradeBase>();
     public int activationCost = 5;
-    public TMP_Text dialogText;
+    public TMP_Text traderText;
     public bool isPayed = false;
-
+    
     void Start()
     {
         
-        dialogText.text = "Hello! I'm a trader. I can offer you the "+ availableUpgrades[0].name +" for: " + activationCost+ " Currcuit Boards".ToLower();
-        dialogText.enabled = false;
     }
+
     private void Update()
     {
         Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, 3f);
-        dialogText.enabled = false;
         foreach (var hitCollider in hitColliders)
         {
            
-            Debug.Log("Collider gefunden: " + hitCollider.name);
+            
             if (hitCollider.CompareTag("Player"))
             {
-                Debug.Log("Spieler gefunden");
-                dialogText.enabled = true;
+                traderText.gameObject.SetActive(true);
+            
                 if (Input.GetKeyDown(KeyCode.E))
                 {
-                    if (isPayed)
-                    {
-                        dialogText.text = "You already bought an upgrade!".ToLower();
-                    }
-                    else
-                    {
-                        if (FindAnyObjectByType<Purse>().GetFunds() >= activationCost)
-                        {
-                            FindAnyObjectByType<Purse>().SubCurrency(activationCost);
-                            dialogText.text = "Thank you for your purchase!".ToLower();
-                            isPayed = true;
-                            foreach (var upgrade in availableUpgrades)
-                            {
-                                upgrade.playerController = FindObjectOfType<PlayerController>();
-                                List<UpgradeBase> playerUpgrades = upgrade.playerController.upgrades;
-                                UpgradeBase current = playerUpgrades.FirstOrDefault(x => x.id == upgrade.id);
-                                if (current != null)
-                                {
-                                    current.unlocked = true;
-                                }
-                                else
-                                {
-                                    //Not found
-                                    Debug.Log("Upgrade not found");
-                                }
-                            }
-                        }
-                        else
-                        {
-                            dialogText.text = "You don't have enough Currcuit Boards!".ToLower();
-                        }
-                    }
+                    GameManager2.Instance.JustPauseTheGame();
+                  TraderUI.Instance.LoadItems(availableUpgrades.ToArray());
+                  TraderUI.Instance.Show();
                 }
             }
           
---
+++ Prototype/Assets/Scripts/Trader/TraderUI.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using TMPro;
+using JetBrains.Annotations;
+[System.Serializable]
+public class UpgradeUI
+{
+    public UpgradeBase upgrade;
+    public TMP_Text upgradeName;
+    public TMP_Text upgradeDescription;
+    public TMP_Text upgradeCost;
+    public GameObject upgradeUI;
+    public Sprite upgradeSprite;
+    public int cost;
+    public bool unlocked = false;
+    
+}
+public class TraderUI : MonoBehaviour
+{
+
+    public static TraderUI Instance;
+    public TMP_Text balanceText;
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    public UpgradeUI[] upgradeUIs;
+    public TMP_Text traderName;
+    public TMP_Text traderMessage;
+    public GameObject traderUI;
+    public void Hide()
+    {
+        traderUI.SetActive(false);
+        GameManager2.Instance.JustPauseTheGame();
+    }
+    public void SetSelectedUpgrage(int index)
+    {
+        UpgradeUI upgradeItemUI = upgradeUIs[index];
+        upgradeItemUI.upgradeName.text = upgradeItemUI.upgrade.name;
+        upgradeItemUI.upgradeDescription.text = upgradeItemUI.upgrade.description;
+    }
+    public void Show()
+    {
+        traderUI.SetActive(true);
+    }
+    public void LoadItems(UpgradeBase[] upgrades)
+    {
+        balanceText.text = FindAnyObjectByType<Purse>().GetFunds().ToString();
+        upgrades = upgrades.OrderBy(upgrade => upgrade.id).ToArray();
+        for(int i = 0; i < upgradeUIs.Length; i++)
+        {
+            upgradeUIs[i].upgrade = upgrades[i];
+            upgradeUIs[i].upgradeName.text = upgrades[i].name;
+            upgradeUIs[i].upgradeDescription.text = upgrades[i].description;
+            upgradeUIs[i].upgradeCost.text = upgrades[i].activationCost.ToString();
+            upgradeUIs[i].cost = upgrades[i].activationCost;
+            if (upgradeUIs[i].unlocked)
+            {
+                upgradeUIs[i].upgradeUI.SetActive(false);
+            }
+            else
+            {
+                upgradeUIs[i].upgradeUI.SetActive(true);
+            }
+            
+        }
+    }
+    public void UnlockItem(int index)
+    {
+        UpgradeUI upgradeItemUI = upgradeUIs[index];
+        if (FindAnyObjectByType<Purse>().GetFunds() >= upgradeItemUI.cost)
+        {
+            FindAnyObjectByType<Purse>().SubCurrency(upgradeItemUI.cost);
+            upgradeItemUI.upgradeUI.SetActive(false);
+            upgradeItemUI.unlocked = true;
+
+            
+
+            UpgradeBase upgrade = GameManager2.Instance.spawnedChar.GetComponent<PlayerController>().upgrades.FirstOrDefault(upgrade => upgrade.id == upgradeUIs[index].upgrade.id);
+            upgrade.unlocked = true;
+        }
+    }
+
+    public void SetTraderName(string name)
+    {
+        traderName.text = name;
+    }
+    public void SetTraderMessage(string message)
+    {
+        traderMessage.text = message;
+    }
+}
