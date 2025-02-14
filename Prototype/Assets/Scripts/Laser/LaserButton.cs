e4e46ec47d5e2bef8ea69b2bb73253828dabb97d
Daniel Nold <75209804+Dnold@users.noreply.github.com>
2025-02-14 19:41:22 +0100

    Added Button Animation

--- Prototype/Assets/Scripts/Laser/LaserButton.cs
+++ Prototype/Assets/Scripts/Laser/LaserButton.cs
@@ -3,11 +3,35 @@ using UnityEngine;
 public class LaserButton : MonoBehaviour
 {
     public Timer timer;
+    bool isPressed = false;
+    public Sprite notPressedSprite;
+    public Sprite pressedSprite;
+    int invokeCount = 0;
+    private void Start()
+    {
+        timer.timerEnded += ResetButton;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            StartCoroutine(timer.StartTimer());
+            if (!isPressed)
+            {
+                GetComponent<SpriteRenderer>().sprite = pressedSprite;
+                isPressed = true;
+                timer.StartCoroutine(timer.StartTimer());
+            }
+           
         }
+    }
+    private void ResetButton()
+    {
+        invokeCount++;
+        if (invokeCount % 2 == 0)
+        {
+            GetComponent<SpriteRenderer>().sprite = notPressedSprite;
+            isPressed = false;
+        }
+        
     }
 }
