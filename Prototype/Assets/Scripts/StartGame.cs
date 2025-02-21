f8c265f25e6691d1b7f9a54068fd1e416867442b
Daniel Nold <75209804+Dnold@users.noreply.github.com>
2025-02-21 15:06:36 +0100

    Main Menu finished

--- Prototype/Assets/Scripts/StartGame.cs
+++ Prototype/Assets/Scripts/StartGame.cs
@@ -1,8 +1,20 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
+    public AudioMixerGroup musicMixerGroup;
+    public AudioMixerGroup sfxMixerGroup;
+    public void Start()
+    {
+        float amount = PlayerPrefs.GetFloat("MusicVolume");
+        musicMixerGroup.audioMixer.SetFloat("MusicVolume", amount);
+
+
+        float amount2 = PlayerPrefs.GetFloat("SFXVolume");
+        sfxMixerGroup.audioMixer.SetFloat("SFXVolume", amount2);
+    }
     public void StartTheGame()
     {
         Destroy(UIManager.instance.gameObject);
