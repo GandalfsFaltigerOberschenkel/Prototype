95bd3a82f5e03386b20d268a2c30b9f2f859ef2f
Daniel Nold <75209804+Dnold@users.noreply.github.com>
2025-03-11 09:33:24 +0100

    Fixed Some Minor stuff

--- Prototype/Assets/Scripts/DestructablePlatforms/DestructablePlatform.cs
+++ Prototype/Assets/Scripts/DestructablePlatforms/DestructablePlatform.cs
@@ -12,30 +12,48 @@ public class DestructablePlatform : MonoBehaviour
     [SerializeField]
     public List<PlatformState> states;
     public SpriteRenderer spriteRenderer;
-    
+    float timer = 0;
+    public float bufferTime = 0.5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.sprite = states[0].sprite;
+        timer = bufferTime;
     }
 
     // Update is called once per frame
+    private void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.collider.tag == "Player")
+        if (collision.collider.tag == "Player")
         {
-            
 
-            if(states.Count >= 1)
+            if (timer <= 0)
             {
-                spriteRenderer.sprite = states[0].sprite;
-                states.RemoveAt(0);
-                
-            }
-            else
-            {
-                Destroy(gameObject);
+                timer = bufferTime;
+
+
+                if (states.Count >= 1)
+                {
+                    spriteRenderer.sprite = states[0].sprite;
+                    states.RemoveAt(0);
+
+
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
             }
         }
     }
